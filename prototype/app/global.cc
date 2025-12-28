687bb53b423934efef54a20636c26835c08a4668
sungkyun123 <sungkyun123@naver.com>
2025-12-28 18:10:52 +0000

    first commit

--- prototype/app/global.cc
+++ prototype/app/global.cc
@@ -4,14 +4,15 @@ char *PlacementName = nullptr;
 double GpThreshold = 0.091;               // for GC (how many invalid pages)
 double OpRatio     = 0.10;                // over-provisioning ratio (physical = logical * (1 + OpRatio))
 int LogicalSizeGb  = 8;                   // logical workload size in GB
-char wk_name[128]  = "/home/nkgy/fio_bench/test-fio-small";
+char wk_name[128]  = "/home/dogi-ae/dogi-prototype/prototype/app/workload/test-fio-small";
+//char wk_name[128]  = "/test-fio-small";
 uint32_t NumGroup  = 6;
 uint64_t BIR[10]   = {0,};
 uint32_t naive_start = 1;
 int APPLY_ML = 0;
 
-const char kZnsDevicePath[] = "/dev/nvme2n2";
-const char kZbdDeviceName[] = "nvme2n2";
+const char kZnsDevicePath[] = "/dev/nvme0n1";
+const char kZbdDeviceName[] = "nvme0n1";
 
 const uint64_t kBlockBytes    = 4096;
 const uint64_t kSegmentBlocks = 16384; // blocks per segment
